a3155c8f73ff18474a62b7ab08fffb434b0714da
shiew1111 <43846075+shiew1111@users.noreply.github.com>
2019-03-16 22:32:14 +0100

    Create Robcio.py

---
+++ Robcio.py
@@ -0,0 +1,25 @@
+import re
+
+try:
+    file_path = input('Writhe Path')
+    plik = open(file_path)
+    y = plik.readlines()
+except:
+    print("Nie ma takiego pliku")
+    exit()
+tablica = []
+for n in range(len(y)):
+    x = re.findall('\d\d[:]\d\d',str(y[n]))
+    tablica+=x
+tablica = [x for x in tablica if x != []]
+minuty = 0
+print(tablica)
+for tm in tablica:
+    podzielCzas = [int(s) for s in tm.split(':')]
+    minuty += (podzielCzas[0] * 60 + podzielCzas[1])
+godz, min = divmod(minuty, 60)
+dzien, godz = divmod(godz,24)
+if dzien < 10:
+    print("{} dzień i {}:{}".format(dzien,godz,min))
+else:
+    print("{} dni i {}:{}".format(dzien, godz, min))
